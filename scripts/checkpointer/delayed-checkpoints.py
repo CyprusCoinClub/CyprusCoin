d0635517dae4a9267163db3b3fabc938ef16b309
DevAurelius <devaurelius@gmail.com>
2019-04-27 13:21:59 +0800

    fix up checkpoint scripts

--- scripts/checkpointer/delayed-checkpoints.py
+++ scripts/checkpointer/delayed-checkpoints.py
@@ -5,7 +5,7 @@ import sys
 import json
 import requests
 
-topbuffer = 3 * 24 * 60 * 2
+topbuffer = 3 * 24 * 60 * 1
 
 
 def lastknownblock():
@@ -19,10 +19,10 @@ def lastknownblock():
 
 
 def height():
-    base_url = 'http://localhost:11898/getheight'
+    base_url = 'http://localhost:11975/getheight'
     resp = requests.get(base_url).json()
     if 'height' not in resp:
-        print ('Unexpected response, make sure TurtleCoind is running',
+        print ('Unexpected response, make sure CyprusCoind is running',
                resp)
         sys.exit(-1)
     else:
@@ -30,7 +30,7 @@ def height():
 
 
 def rpc(method, params={}):
-    base_url = 'http://localhost:11898/json_rpc'
+    base_url = 'http://localhost:11975/json_rpc'
     payload = {
         'jsonrpc': '2.0',
         'id': 'block_info',
@@ -39,7 +39,7 @@ def rpc(method, params={}):
         }
     resp = requests.post(base_url, data=json.dumps(payload)).json()
     if 'result' not in resp:
-        print ('Unexpected response, make sure Turtlecoind is running with block explorer enabled'
+        print ('Unexpected response, make sure CyprusCoind is running with block explorer enabled'
                , resp)
         sys.exit(-1)
     else:
@@ -64,13 +64,13 @@ while current_height > stop_height:
     try:
         blocks = get_block_info(current_height)
         for b in blocks:
-            print '%(height)s,%(hash)s' % b
+            print("%(height)s,%(hash)s" % b)
             all_blocks.append('%(height)s,%(hash)s' % b)
             current_height = b['height'] - 1
             if current_height < stop_height:
                 break
     except:
-        print "Whoops... let's try that again"
+        print("Whoops... let's try that again")
 
 all_blocks.reverse()
 
