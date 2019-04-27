598ab9a5b7f071579917e0e396744c26edabc01d
DevAurelius <devaurelius@gmail.com>
2019-04-27 13:30:54 +0800

    double the number of internal checkpoints
    as CyprusCoin targets 1 block per min vs 2 blocks
    of TurtleCoin

--- scripts/internal-checkpoints-tool/checkpoints.js
+++ scripts/internal-checkpoints-tool/checkpoints.js
@@ -5,7 +5,7 @@ var util = require('util')
 lr.on('line', (line) => {
   var p = line.split(',')
   p[0] = parseInt(p[0])
-  if (p[0] % 5000 === 0) {
+  if (p[0] % 2500 === 0) {
     console.log(util.format('{%s, "%s"},', p[0].toString().padStart(8), p[1]))
   }
 })
